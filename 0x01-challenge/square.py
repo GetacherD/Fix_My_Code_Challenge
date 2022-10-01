2a35cf238f726a9667fb7b7109617d77515a3d2b
GetacherD <getacher.d@gmail.com>
2022-10-01 20:42:08 +0300

    fix code style

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
 class square():
-    
-    width = 0
-    height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        self.width = 0
+        self.height = 0
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -19,6 +17,7 @@ class square():
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
