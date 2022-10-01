587c7d49372255288a2a062746bf97c3115ea551
GetacherD <getacher.d@gmail.com>
2022-10-01 21:18:51 +0300

    add documentation

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+"""
+Square module
+"""
+
 
 class Square():
 
-    def __init__(self, *args, **kwargs):
+    """ Square object Representation"""
+    def __init__(self, **kwargs):
+        """ initialize new square object"""
         self.width = 0
         self.height = 0
         for key, value in kwargs.items():
@@ -13,6 +19,7 @@ class Square():
         return self.width * self.height
 
     def permiter_of_my_square(self):
+        """ Get permiter_of_my_square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
@@ -20,7 +27,6 @@ class Square():
 
 
 if __name__ == "__main__":
-
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
